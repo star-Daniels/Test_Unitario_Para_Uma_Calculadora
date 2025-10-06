36cdea98877619664506e94d68043e40c279638d
Daniel Santos <166452209+star-Daniels@users.noreply.github.com>
2025-10-06 20:57:09 -0300

    Add files via upload

---
+++ MeuNovoProjeto/src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Cauculadora calculadora = new Cauculadora();
+
+        System.out.print(calculadora.somar(10,20));
+
+    }
+}
