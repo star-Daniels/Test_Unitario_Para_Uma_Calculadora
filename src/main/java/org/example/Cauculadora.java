3bccbec751ac09fd994dfb4ccc83bf587a17034b
Daniel Santos <166452209+star-Daniels@users.noreply.github.com>
2025-10-06 20:57:57 -0300

    Add files via upload

---
+++ src/main/java/org/example/Cauculadora.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class Cauculadora {
+    public double somar(double a, double b){
+        return a+b;
+    }
+
+    public double diminuir(double a, double b){
+        return a-b;
+    }
+
+    public double multiplicar(double a, double b){
+        return a*b;
+    }
+
+    public double dividir(double a, double b){
+        return a/b;
+    }
+}
